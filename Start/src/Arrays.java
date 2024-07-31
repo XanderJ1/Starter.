3d605c0e8183ae8f4043d85473d9e2a4257464b6
Bash <98415213+XanderJ1@users.noreply.github.com>
2024-07-31 23:50:27 +0000

    Delete Start/src directory

--- Start/src/Arrays.java
+++
@@ -1,8 +0,0 @@
-public class Arrays {
-    public static void main(String[] args) {
-        int[] array = {1,2,22,44,55,44,33,44,55,11};
-        for (int i=0; i<10; i++){
-            System.out.println(array[i]);
-        }
-    }
-}
--- Start/src/bash/CommandLine.java
+++
@@ -1,9 +0,0 @@
-package bash;
-
-public class CommandLine{
-    public static void main(String[] args) {
-        for (int index = 0; index < args.length; index++){
-            System.out.println(args[index]);
-        }
-    }
-}
